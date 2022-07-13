34bd641d79069a1d70128f00a6fc4993bb77baf5
Vinícius de Alcantra <52285659+Vini03-Alcantra@users.noreply.github.com>
2022-07-12 23:45:39 -0300

    Ensure LocalLoadPurchases calls cacheStore.fetch with correct key

---
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -0,0 +1,31 @@
+import {LocalLoadPurchases} from "../../usecases/"
+import {CacheStoreSpy} from "../../tests"
+
+type SutTypes = {
+    sut: LocalLoadPurchases;
+    cacheStore: CacheStoreSpy
+}
+
+const makeSut = (timestamp = new Date()): SutTypes => {
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalLoadPurchases(cacheStore, timestamp)
+    
+    return {
+        sut,
+        cacheStore
+    }
+}
+
+describe("LocalSavePurchases", () => {
+    test('Should not delete or insert cache on sut.init', () => {
+        const {cacheStore} = makeSut()        
+        expect(cacheStore.actions).toEqual([])
+    })
+
+    test('Should call correct key on load', async () => {
+        const {cacheStore, sut} = makeSut()
+        await sut.loadAll()
+        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch])
+        expect(cacheStore.fetchKey).toBe('purchases')
+    })
+})
